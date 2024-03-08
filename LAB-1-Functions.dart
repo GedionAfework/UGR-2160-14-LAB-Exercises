78b0ddf0e997c96beafde18c58a70b52795c1d63
GedionAfework <gedionmekbeb2@gmail.com>
2024-03-08 16:12:14 +0300

    Done exercise 1 of Functions

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,9 @@
+int sum(int first, int second) {
+  return first + second;
+}
+void main() {
+  int num1 = 5;
+  int num2 = 10;
+  int result = sum(num1, num2);
+  print("$num1 + $num2 = $result");
+}
