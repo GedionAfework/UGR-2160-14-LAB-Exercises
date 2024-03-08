e2bc13b3cf2aac552727b6888946bcde149fafed
GedionAfework <gedionmekbeb2@gmail.com>
2024-03-08 15:33:19 +0300

    Exercise 1 of Variables and types done

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Gedion";
+  int age = 20;
+  String favColor = "Orange";
+  print("My name is $name.");
+  print("I am $age years old");
+  print("My favorite color is $favColor.");
+}
